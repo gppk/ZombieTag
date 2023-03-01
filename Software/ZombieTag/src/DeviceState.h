716bab2d628ceaf1e84b8c6965cd2fbeb13baa09
Alex Eastabrook <Alex.eastabrook@newburyinnovation.co.uk>
2023-03-01 17:03:30 +0000

    Add @GPPK's Current Starting Point.

---
+++ Software/ZombieTag/src/DeviceState.h
@@ -0,0 +1,41 @@
+#pragma once
+
+enum DeviceState {
+    initialising,
+    human,
+    infected,
+    medicalzone,
+    null
+};
+
+const char* DeviceStateToString( DeviceState ds ) {
+    switch ( ds )
+    {
+        case DeviceState::initialising: return "initialising";
+        case DeviceState::human: return "human";
+        case DeviceState::infected: return "infected";
+        case DeviceState::medicalzone: return "medical zone";
+        case DeviceState::null: return "null";
+        default: return "Default";
+    }
+}
+
+DeviceState StringToDeviceState (String s) {  
+    if ( s == "initialising" ) return DeviceState::initialising;
+    if ( s == "human" ) return DeviceState::human;
+    if ( s == "infected" ) return DeviceState::infected;
+    if ( s == "medical zone" ) return DeviceState::medicalzone;
+    // else
+    return DeviceState::null;
+}
+
+// Not sure if this deserves to be in here or Lora_Manager.h but probs here.
+bool is_message_ztag ( String recievedLoraMessage ) {
+    bool isZtag = false;
+
+    if ( StringToDeviceState(recievedLoraMessage) != DeviceState::null ) {
+        isZtag = true;
+    }
+    
+    return isZtag;
+}
---
+++ Software/ZombieTag/src/Lora_Manager.h
@@ -0,0 +1,72 @@
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+
+struct ReceivedPacket { 
+  int rssi;           
+  String message;
+};
+ReceivedPacket receivedPacket;
+
+bool init_lora() {
+    bool ret = false;
+    //SPI LoRa pins
+    SPI.begin(SCK, MISO, MOSI, SS);
+    //setup LoRa transceiver module
+    LoRa.setPins(SS, RST, DIO0);
+
+    if (LoRa.begin(BAND)) {
+        ret = true;
+    }
+    return ret;
+}
+
+// Also stores into the recievedPacket storage object for later retrieval
+// but returns string so we can get an instantaneous response for the packet
+// rather than trusting the storage
+String get_new_lora_packet() {
+    // Get a packet - needs its own function
+    bool packetFound = false;
+    String loraData;
+
+    while( !packetFound ) {
+        //try to parse packet
+        int packetSize = LoRa.parsePacket();
+        if ( packetSize ) {
+            //received a packet
+            Serial.println("###############");
+            Serial.print("Received packet: ");
+
+            //read packet
+            while (LoRa.available()) {
+                loraData = LoRa.readString();
+                Serial.println(loraData);
+            }
+
+            receivedPacket.rssi = LoRa.packetRssi();
+            receivedPacket.message = loraData;
+            Serial.println("Data in RSSI Packet");
+            Serial.print("Message: ");
+            Serial.println(receivedPacket.message);
+            Serial.print("RSSI: ");
+            Serial.println(receivedPacket.rssi);
+            Serial.println("###############");
+            packetFound = true;
+        }
+    }
+    return receivedPacket.message;
+}
